e9f260ed77346bab9c533541b1ac0968d850cc18
Josué Estévez Fernández <jestfer6@gmail.com>
2018-04-21 00:04:06 +0200

    added Host Binding to count-clicks directive, + opacity as clickN grows

--- src/app/directives/count-clicks.directive.ts
+++ src/app/directives/count-clicks.directive.ts
@@ -1,11 +1,13 @@
-import { Directive, HostListener } from "@angular/core";
+import { Directive, HostListener, HostBinding } from "@angular/core";
 
 @Directive({
-  selector: 'a[count-clicks]'
+  selector: 'li[count-clicks]'
 })
 export class CountClicksDirective {
   clickN = 0
+  @HostBinding('style.opacity') opacity: number = .1
   @HostListener('click', ['$event.target']) onclick(btn){
-    console.log('a', btn, "Number of clicks:", this.clickN++);
+    console.log('a', btn, "Number of clicks:", this.clickN++)
+    this.opacity += .1
   }
 }

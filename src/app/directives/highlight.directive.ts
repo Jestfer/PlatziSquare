ae716f97a5f9bb6c6ada066d0fe2d43f84557655
Josué Estévez Fernández <jestfer6@gmail.com>
2018-04-20 12:47:50 +0200

    created custom directive 'highlight' for premium businesses

---
+++ src/app/directives/highlight.directive.ts
@@ -0,0 +1,16 @@
+import { Directive, OnInit, ElementRef, Renderer2, Input } from "@angular/core";
+
+@Directive({
+  selector: '[highlight]'
+})
+export class HighlightDirective implements OnInit{
+  constructor(private elRef: ElementRef, private renderer: Renderer2){}
+  @Input('highlight') plan: string = ''
+
+  ngOnInit() {
+    if(this.plan === 'premium'){
+      this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'yellow')
+      this.renderer.setStyle(this.elRef.nativeElement, 'font-weight', 'bold')
+    }
+  }
+}

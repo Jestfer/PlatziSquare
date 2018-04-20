88b216e6530ae450eacdb1cbb32a576d95b0c223
Josué Estévez Fernández <jestfer6@gmail.com>
2018-04-20 12:25:38 +0200

    example of ngSwitch and ngSwitchCase to show proximity of businesses

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,12 +8,12 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'PlatziSquare'
   places:any = [
-    { active: true, name: 'Makers Academy'},
-    { active: true, name: 'Misty Flower'},
-    { active: false, name: 'Ricos Tacos'},
-    { active: true, name: 'Ritz Hotel'},
-    { active: true, name: 'Roast and Toast'},
-    { active: true, name: 'La Habana'}
+    { active: true, name: 'Makers Academy', proximity: 1, distance: 0 },
+    { active: true, name: 'Roast and Toast', proximity: 1, distance: 0.5 },
+    { active: true, name: 'Misty Flower', proximity: 2, distance: 1 },
+    { active: false, name: 'Ricos Tacos', proximity: 2, distance: 2 },
+    { active: true, name: 'Ritz Hotel', proximity: 3, distance: 10 },
+    { active: true, name: 'La Habana', proximity: 3, distance: 30 }
   ]
   lat:number = 51.5173403
   lng:number = -0.0754695

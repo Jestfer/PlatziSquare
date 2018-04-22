3b7ee459cb002a2f64d3de4ef4bd72dd0c13b4c9
Josué Estévez Fernández <jestfer6@gmail.com>
2018-04-22 17:48:06 +0200

    set up Firebase, hid away keys in environment.ts - :bug: Firebase 4.8.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,12 +17,24 @@ import { CountClicksDirective } from './directives/count-clicks.directive';
 import { Routes, RouterModule } from "@angular/router";
 import { PlacesService } from './services/places.service';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule, AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
 const appRoutes: Routes = [
   { path: '', component: PlacesComponent },
   { path: 'places', component: PlacesComponent },
   { path: 'contact', component: ContactComponent },
   { path: 'detail/:id', component: DetailComponent }
 ]
+
+export const firebaseConfig = {
+  apiKey: environment.firebaseConfig.apiKey,
+  authDomain: environment.firebaseConfig.authDomain,
+  databaseURL: environment.firebaseConfig.databaseURL,
+  storageBucket: environment.firebaseConfig.storageBucket,
+  messagingSenderId: environment.firebaseConfig.messagingSenderId
+};
 
 @NgModule({
   declarations: [
@@ -40,7 +52,10 @@ const appRoutes: Routes = [
     AgmCoreModule.forRoot({
       apiKey: environment.apiAGM
     }),
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   providers: [PlacesService],
   bootstrap: [AppComponent]
